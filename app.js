0cd5e9cd2a9b2eb33b437dfdd7af4395efba82cd
jhoanVasquez <jhoan.vasquez@correounivalle.edu.co>
2021-01-20 15:38:27 -0500

    Revert "JWT"
    This reverts commit d7aecdf4d2ec276f5b5431fc9dec2fcd3b774aa6.

--- app.js
+++ app.js
@@ -1,68 +1,25 @@
 const express = require('express');
 const mysql = require('mysql2');
-var jwt=require('jsonwebtoken');
-
 const app = express();
 
 
 //Ruta para obtener los registros de la BD
 app.get("/", function(req, res){
-  
-    //Creacion de token
-    var tokenData = {
-      username: "username"
-      // ANY DATA
-    }
-  
-    var token = jwt.sign(tokenData, 'Secret Password', {
-      expiresIn: 60 * 60 * 24 // expires in 24 hours
-    })
-  
-    res.send({
-      token
-    })
-      //res.json(results);
-    
-   
-  
-});
-
-app.get('/stations', (req, res) => {
   const sql = 'select * from estaciones'
-  var token = req.headers['authorization']
-  if(!token){
-      res.status(401).send({
-        error: "Es necesario el token de autenticación"
-      })
-      return
-  }
-
-  token = token.replace('Bearer ', '')
-
   connection.query(sql,(error, results)=>{
     if(error){
       console.error('error : ' + err.stack);
     }
 
     if(results.length > 0){
-      
-      jwt.verify(token, 'Secret Password', function(err, user) {
-        if (err) {
-          res.status(401).send({
-            error: 'Token inválido'
-          })
-        } else {
-          res.json(results);
-        }
-      })
-          
+      res.json(results);
     }
     else{
-        res.send('No hay resultados');
+      res.send('No hay resultados');
     }
   });
-  
-})
+});
+
 
 //MySql
 
